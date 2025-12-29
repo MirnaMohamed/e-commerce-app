85df020b028872b54d58e7786036d1370ee595e4
MirnaMohamed <mirna@techspire-solutions.com>
2025-12-29 04:32:15 +0200

    viewing product cards

---
+++ app/products/ProductCard.tsx
@@ -0,0 +1,18 @@
+import { Product } from "@/lib/mocks";
+import Image from "next/image";
+
+export function ProductCard( {product}: {product: Product} ) {
+    return (
+        <div className="border rounded-lg p-4">
+            <div className="relative aspect-video">
+            <Image src={product.image}
+             alt={product.name} fill
+             sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw"
+             className="object-cover" />
+             </div>
+            <h2 className="text-xl font-semibold mb-2">{product.name}</h2>
+            <p className="text-gray-700 mb-2">${product.price.toFixed(2)}</p>
+            <p className="text-gray-600">{product.description}</p>
+        </div>
+    );
+}
