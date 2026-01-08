ed59498b5affac432ff978c1c8e1622f0cc57c4d
MirnaMohamed <mirnaattallah00@gmail.com>
2026-01-08 02:11:29 +0200

    Use shadcn skeleton for loading products

--- app/loading.tsx
+++ app/loading.tsx
@@ -1,6 +1,17 @@
+import { Skeleton } from "@/components/ui/skeleton";
+import { ProductCardSkeleton } from "./ProductCardSkeleton";
 
 export default function Loading() {
-    return <div className="flex items-center justify-center h-screen">
-        <p className="text-lg">Loading...</p>
-    </div>
+    return (
+        <main className="container mx-auto p-4">
+          <h1 className="text-3xl font-bold mb-6">Home</h1>
+          <p>Showing 5 products</p>
+          <div className="grid grid-cols-1 gap-4 md:grid-cols-2 lg:grid-cols-3">
+            { Array.from({ length: 6 }).map((_, i) => (
+              <ProductCardSkeleton key={i} />
+            ))
+            }
+          </div>
+        </main>
+        );
 }
