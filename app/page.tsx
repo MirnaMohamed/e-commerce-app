f62b0d5546a9d91dc18c81acca27f5f4f79ac664
MirnaMohamed <mirnaattallah00@gmail.com>
2026-01-08 02:46:49 +0200

    use shadcn pagination

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,6 @@
 import db from "@/lib/prisma";
 import { ProductCard } from "./products/ProductCard";
+import { Pagination, PaginationContent, PaginationEllipsis, PaginationItem, PaginationLink, PaginationNext, PaginationPrevious } from "@/components/ui/pagination";
 
 type SearchParams = Promise<{ [key: string]: string | string[] | undefined }>;
 
@@ -8,12 +9,14 @@ export default async function Home(props : { searchParams: SearchParams }) {
   const page = Number(searchParams.page) || 1;
   const pageSize = 3;
   const skip = (page - 1) * pageSize;
-  const products = await db.product.findMany({
-    skip,
-    take: pageSize,
-    orderBy: { id: "asc" },
-  });
-  console.log(products);
+  const [products, total] = await Promise.all([
+    db.product.findMany({
+      skip: skip,
+      take: pageSize,
+    }),
+    db.product.count()
+  ]) 
+  const totalPages = Math.ceil(total / pageSize);
   return (
     <main className="container mx-auto p-4">
       <h1 className="text-3xl font-bold mb-6">Home</h1>
@@ -24,6 +27,28 @@ export default async function Home(props : { searchParams: SearchParams }) {
         ))
         }
       </div>
+      <Pagination className="mt-6">
+      <PaginationContent>
+        <PaginationItem>
+          <PaginationPrevious href={`?page=${page - 1}`} />
+        </PaginationItem>
+
+        {Array.from({ length: totalPages }, (_, i) => 
+          <PaginationItem key={i}>
+            <PaginationLink
+              href={`?page=${i+1}`}
+              isActive={i+1 === page}
+            >
+              {i+1}
+            </PaginationLink>
+          </PaginationItem>
+        )}
+
+        <PaginationItem>
+          <PaginationNext href={`?page=${page+1}`} />
+        </PaginationItem>
+      </PaginationContent>
+    </Pagination>
     </main>
     );
 }
